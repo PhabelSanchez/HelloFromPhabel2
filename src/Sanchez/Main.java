bf49dd8bdb2634879e4c2bce92da882d4d052c71
PhabelSanchez <phabel.sanchez@gmail.com>
2021-05-22 22:19:10 -0600

    adding another line before erase Emma

--- src/Sanchez/Main.java
+++ src/Sanchez/Main.java
@@ -7,6 +7,7 @@ public class Main {
         System.out.println("Hello my name is Phabel");
         System.out.println("Hello Phabel, my name is Ema");
         System.out.println("This is an extra line for your stretch challenge if you want");
+        System.out.println("Thanks Emma, yes I will erase that line to prove my stretch challenge");
     }
 }
 
